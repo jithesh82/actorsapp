f0bb3902abf5dd46e67f3f6245af1fe743a900bd
jithesh82 <jithesh82@gmail.com>
2022-03-06 13:55:18 -0500

    updating guidepost.py, adding test speech file speak_the_speech.txt

--- guidepost.py
+++ guidepost.py
@@ -4,7 +4,9 @@
 import nltk
 import random
 
+# creating a list of words to be removed
 stopw = nltk.corpus.stopwords.words('english')
+# adding 'I' to the list
 stopw.append('I')
 
 #file = 'textguidepost.txt'
@@ -12,16 +14,16 @@ file = 'speak_the_speech.txt'
 
 for line in  open(file): 
     #print(line)
+    # breaking the line to word list
     words = nltk.word_tokenize(line)
+    # removing punctuations
     words = [x for x in words if x.isalnum()]
+    # removing stop words
     words = [x for x in words if x not in stopw]
+    # printing words using random styles, foreground 
+    # and background colors
     for word in words:
-        #import time
-        #random.seed(time.time())
         style = random.randint(0,7)
         fg    = random.randint(31, 37)
         bg    = random.randint(40, 47)
         print(('\x1b[%s;%s;%sm' % (str(style), str(fg), str(40))) + word + '\x1b[0m', end=' ')
-    #print('\x1b[0m')
-    #print(words)
-    #break
